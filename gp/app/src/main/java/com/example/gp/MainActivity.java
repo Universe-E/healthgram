0d7cf100e738b2c24e8c5cf5d123b981b49611dc
Kerry <43082506+Rickzhang512@users.noreply.github.com>
2024-04-18 16:55:59 +1000

    fundamental

--- gp/app/src/main/java/com/example/gp/MainActivity.java
+++ gp/app/src/main/java/com/example/gp/MainActivity.java
@@ -2,17 +2,19 @@ package com.example.gp;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.gp.databinding.ActivityMainBinding;
 
-
 public class MainActivity extends AppCompatActivity {
-    private com.example.gp.databinding.ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 使用 View Binding 加载布局
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-
+        // 将布局的根视图设置为当前活动的内容视图
+        setContentView(binding.getRoot());
     }
 }
