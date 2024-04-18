c7f35c44bb77ddde6c20ecf08016e99853c0b158
chenyulong <u7756137@anu.edu.au>
2024-04-18 17:00:32 +1000

    main.xml部分id修改

--- gp/app/src/main/java/com/example/gp/MainActivity.java
+++ gp/app/src/main/java/com/example/gp/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.gp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gp.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private com.example.gp.databinding.ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding.btnLogin.setOnClickListener(this);
+        binding.btnCreateAccount.setOnClickListener(this);
 
+    }
 
+    @Override
+    public void onClick(View v) {
+        if(v == binding.btnLogin) {
+            // login
+            String username = binding.etUsername.getText().toString();
+        } else if(v == binding.btnCreateAccount) {
+            // create account
+
+        }
     }
 }
