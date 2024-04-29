0a2cf08e03a9de2a3d17457fe61293871498a6a2
Zehua Kong <u7693498@anu.edu.au>
2024-04-29 14:49:35 +1000

    Setup: firebase connect

--- gp/app/build.gradle.kts
+++ gp/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-auth")
 
 
     // TODO: Add the dependencies for Firebase products you want to use
