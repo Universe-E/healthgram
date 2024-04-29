d6b9212ae9d3f12e054cbc6ce28f5d88511a19d9
Zehua Kong <u7693498@anu.edu.au>
2024-04-29 19:59:02 +1000

    Add: firebase implementation on SignUpActivity.java, rewrite create account logic

--- gp/app/src/main/java/com/example/gp/SignUpActivity.java
+++ gp/app/src/main/java/com/example/gp/SignUpActivity.java
@@ -1,24 +1,110 @@
 package com.example.gp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText pwdEditText;
+    private EditText reapeatPwdEditText;
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize views
+        usernameEditText = findViewById(R.id.user_name_text);
+        emailEditText = findViewById(R.id.email_text);
+        pwdEditText = findViewById(R.id.pwd_text);
+        reapeatPwdEditText = findViewById(R.id.repeat_pwd_text);
+        createAccountButton = findViewById(R.id.tv_create_account);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String password = pwdEditText.getText().toString().trim();
+                String repeat_password = reapeatPwdEditText.getText().toString().trim();
+                if (password.equals(repeat_password)) {
+                    createUserAccount(username, email, password);
+                }
+                else {
+                    //password and repeat password are not match
+                    Toast.makeText(SignUpActivity.this, "Repeat password doesn't match",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+    }
+
+    private void createUserAccount(String username, String email, String password) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // User account created successfully
+                            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            saveUserData(userId, username, email);
+                            // Redirect to Main Activity after successful create
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+
+
+                        } else {
+                            // Account creation failed
+                            Toast.makeText(SignUpActivity.this, "Sign Up Failed: " + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void saveUserData(String userId, String username, String email) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", username);
+        user.put("email", email);
+
+        FirebaseFirestore.getInstance().collection("users").document(userId)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(SignUpActivity.this, "Sigun up success", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUpActivity.this, "Save data failed: " + e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
