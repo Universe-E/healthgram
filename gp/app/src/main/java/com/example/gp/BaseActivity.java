18ebd37eb600651bd1999d19a7a1137534796639
Tianci Li <u7773219@anu.edu.au>
2024-05-01 16:14:30 +1000

    add function: toggle nav_bar's button visibility

--- gp/app/src/main/java/com/example/gp/BaseActivity.java
+++ gp/app/src/main/java/com/example/gp/BaseActivity.java
@@ -3,6 +3,7 @@ package com.example.gp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,23 +37,54 @@ public class BaseActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(layoutResId);
         Toolbar top_nav_bar = findViewById(R.id.tb_top_navigation_bar);
         if (top_nav_bar != null) {
+            // Set the top navigation bar as the action bar
             setSupportActionBar(top_nav_bar);
+
+            // Set the title of the activity
+            getSupportActionBar().setTitle("Title");
+
+            // Set the back icon, default is visible
             iv_back = findViewById(R.id.iv_back);
             iv_back.setOnClickListener(this);
+
+            // Set the more icon, default is invisible
+            iv_more = findViewById(R.id.iv_more);
+            iv_more.setOnClickListener(this);
+            iv_more.setVisibility(View.GONE);
         }
     }
 
     @Override
     public void onClick(View v) {
         if(v == iv_back){
-            onBackPressed();
+            // onBackPressed();
+            toggleRightIcon();
+        }
+        else if(v == iv_more){
+            // Do something
+            Toast.makeText(this, "More", Toast.LENGTH_SHORT).show();
         }
     }
-//    public void toggleLeftIcon(){
-//        iv_back.setVisibility(View.GONE);
-//    }
-//
-//    public void toggleRightIcon() {
-//        iv_more.setVisibility(View.GONE);
-//    }
+
+    /*
+    * Toggle the left 'back' icon. Default is visible.
+     */
+    public void toggleLeftIcon(){
+        if(iv_back.getVisibility() == View.VISIBLE){
+            iv_back.setVisibility(View.GONE);
+        }else{
+            iv_back.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /*
+    * Toggle the right 'more' icon. Default is invisible.
+     */
+    public void toggleRightIcon(){
+        if(iv_more.getVisibility() == View.VISIBLE){
+            iv_more.setVisibility(View.GONE);
+        }else{
+            iv_more.setVisibility(View.VISIBLE);
+        }
+    }
 }
