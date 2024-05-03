8b6be289bf884138246021bb4975c9d9b1aca6e0
Tianci <u7773219@anu.edu.au>
2024-05-03 18:39:22 +1000

    Initialization: sharing page

---
+++ gp/app/src/main/java/com/example/gp/data/SharePageActivity.java
@@ -0,0 +1,27 @@
+package com.example.gp.data;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gp.BaseActivity;
+import com.example.gp.R;
+
+public class SharePageActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_share_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
