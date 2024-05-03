a46f3ebed242124579e5de92d6a804ccb62ee83a
Tianci <u7773219@anu.edu.au>
2024-05-03 19:56:46 +1000

    Update: sharing page now shows nav bar

--- gp/app/src/main/java/com/example/gp/data/SharePageActivity.java
+++ gp/app/src/main/java/com/example/gp/data/SharePageActivity.java
@@ -1,6 +1,7 @@
 package com.example.gp.data;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +11,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.gp.BaseActivity;
 import com.example.gp.R;
+import com.example.gp.databinding.ActivitySettingBinding;
 
 public class SharePageActivity extends BaseActivity {
 
+    private final String activityName = "Share with...";
+    private ActivitySettingBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_share_page);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_share_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // initialize top bar
+        setUpTitleBar(R.layout.activity_share_page, activityName);
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        if (v == binding.llSettingChangeAvatarLayout) {
+
+        }
     }
 }
