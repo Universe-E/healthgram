aafdb26a289c6863c546cdf53fa191a90f210c86
Han Bao <u7752342@anu.edu.au>
2024-05-04 22:47:27 +1000

    Added new function Database.user.getUserEmailByUsername

--- gp/app/src/main/java/com/example/gp/data/Database.java
+++ gp/app/src/main/java/com/example/gp/data/Database.java
@@ -2,11 +2,8 @@ package com.example.gp.data;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -15,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Database {
     // User
@@ -51,30 +49,24 @@ public class Database {
             // Get user email by username
             FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-            ArrayList<String> email = new ArrayList<>();
+            AtomicReference<String> email = new AtomicReference<>();
 
             DocumentReference docRef = db.collection("users").document(username);
-            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            email.add(document.getString("email"));
-                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        } else {
-                            Log.d(TAG, "No such document");
-                        }
+            docRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        email.set(document.getString("email"));
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                     } else {
-                        Log.d(TAG, "get failed with ", task.getException());
+                        Log.d(TAG, "No such document");
                     }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
                 }
             });
 
-            if (!email.isEmpty()) {
-                return email.get(0);
-            }
-            return null;
+            return email.get();
         }
     }
 }
