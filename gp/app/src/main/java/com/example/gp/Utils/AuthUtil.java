b34bc27b595d3c65463caca699b142d8a80f81d9
Zehua Kong <u7693498@anu.edu.au>
2024-05-04 22:40:02 +1000

    Add: AuthUtil.java Refactor: firebase.database in build.gradle.kts

--- gp/app/src/main/java/com/example/gp/Utils/AuthUtil.java
+++ gp/app/src/main/java/com/example/gp/Utils/AuthUtil.java
@@ -1,44 +1,70 @@
 package com.example.gp.Utils;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.jetbrains.annotations.Contract;
+
+import java.util.Objects;
 
 
 public class AuthUtil {
 
     private static final String TAG = "Authorization";
+    private static final String USERS_NODE = "users";
 
     /*********** API ***********/
 
     public static boolean isValidEmail(String email) {
-        // TODO: Check if the email is valid (is in the correct format)
-
-        return true;  // TODO: Change this
+        // Check if the email is in the correct format
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        return email.matches(emailPattern);
     }
 
     public static boolean isUsernameTaken(String username) {
-        // TODO: Check if the username is taken
-        // i.e. check if the username is already in the database
+        // Check if the username is already in the database
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child(USERS_NODE);
+        final boolean[] isTaken = {false};
+        usersRef.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                isTaken[0] = dataSnapshot.exists();
+            }
 
-        return false; // TODO: Change this
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "Error checking username availability", databaseError.toException());
+            }
+        });
+        return isTaken[0];
     }
 
     public static boolean isEmailTaken(String email) {
-        // TODO: Check if the email is taken
-        // i.e. check if the email is already in the database
-
-        return false; // TODO: Change this
+        // Check if the email is already in the database
+        final boolean[] isTaken = {false};
+        FirebaseAuth.getInstance().fetchSignInMethodsForEmail(email).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                isTaken[0] = !Objects.requireNonNull(task.getResult().getSignInMethods()).isEmpty();
+            }
+        });
+        return isTaken[0];
     }
 
     public static boolean isValidUsername(String username) {
-        // TODO: Check if the username is available
-
         // i.e. check if the username is toxic
         // i.e. check if the username is spam
         // i.e. check if the username is offensive
-
-        return true; // TODO: Change this
+        return !username.isEmpty() && !username.contains(" ")
+                && !(username.length() <= 18);
     }
 
     public static boolean isEmail(String input) {
@@ -48,9 +74,26 @@ public class AuthUtil {
     @Nullable
     @Contract(pure = true)
     public static String getEmailByUsername(String username) {
-        // TODO: get email by username
+        // Get email by username from the database
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child(USERS_NODE);
+        final String[] email = {null};
+        usersRef.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                        email[0] = userSnapshot.child("email").getValue(String.class);
+                        break;
+                    }
+                }
+            }
 
-        return null; // TODO: Change this
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "Error retrieving email by username", databaseError.toException());
+            }
+        });
+        return email[0];
     }
 
     /********** END API **********/
