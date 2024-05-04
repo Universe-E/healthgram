22238af0693e699507298eb528f183bc144cbd01
Han Bao <u7752342@anu.edu.au>
2024-05-04 14:57:05 +1000

    Restructured SignUpActivity and added AuthUtils for more robust up front check before create account.

--- gp/app/src/main/java/com/example/gp/SignUpActivity.java
+++ gp/app/src/main/java/com/example/gp/SignUpActivity.java
@@ -1,95 +1,105 @@
 package com.example.gp;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.gp.Utils.ToastUtil;
+import com.example.gp.Utils.AuthUtils;
+
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText usernameEditText;
-    private EditText emailEditText;
-    private EditText pwdEditText;
-    private EditText reapeatPwdEditText;
-    private Button createAccountButton;
+    private static final String TAG = SignUpActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+    }
 
+    public void createUserAccount(View view) {
         // Initialize views
-        usernameEditText = findViewById(R.id.user_name_text);
-        emailEditText = findViewById(R.id.email_text);
-        pwdEditText = findViewById(R.id.pwd_text);
-        reapeatPwdEditText = findViewById(R.id.repeat_pwd_text);
-        createAccountButton = findViewById(R.id.tv_create_account);
+        EditText usernameEditText = findViewById(R.id.user_name_text);
+        EditText emailEditText = findViewById(R.id.email_text);
+        EditText pwdEditText = findViewById(R.id.pwd_text);
+        EditText reapeatPwdEditText = findViewById(R.id.repeat_pwd_text);
 
-        createAccountButton.setOnClickListener(v -> {
-            String username = usernameEditText.getText().toString().trim();
-            String email = emailEditText.getText().toString().trim();
-            String password = pwdEditText.getText().toString().trim();
-            String repeat_password = reapeatPwdEditText.getText().toString().trim();
-            if (password.equals(repeat_password)) {
-                createUserAccount(username, email, password);
-            }
-            else {
-                //password and repeat password are not match
-                Toast.makeText(SignUpActivity.this, "Repeat password doesn't match",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        String username = usernameEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
+        String password = pwdEditText.getText().toString().trim();
+        String repeat_password = reapeatPwdEditText.getText().toString().trim();
+
+        if (validateForm(username, email, password, repeat_password)) {
+            // Create account
+            mAuth = FirebaseAuth.getInstance();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                // TODO: Update UI with user information
+                                // TODO: Save user information to Firestore
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                ToastUtil.showLong(SignUpActivity.this, "Authentication failed: " + Objects.requireNonNull(task.getException()).getMessage());
+                            }
+                        }
+                    });
+        }
+
     }
 
-    private void createUserAccount(String username, String email, String password) {
-        //user email as the unique user id
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    // User account created successfully
-                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                    saveUserData(userId, username, email);
-                    // Redirect to Main Activity after successful create
-                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                    startActivity(intent);
+    private boolean validateForm(String username, String email, String password, String repeat_password) {
 
+        if (!password.equals(repeat_password)) {
+            // Passwords don't match
+            ToastUtil.showLong(this, "Repeat password doesn't match");
+        } else if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            // Fields are empty
+            ToastUtil.showLong(this, "Please fill all fields");
+        } else if (password.length() < 6) {
+            // Password is too short
+            ToastUtil.showLong(this, "Password must be at least 6 characters");
+        } else if (!AuthUtils.isValidEmail(email)) {
+            // Email is invalid
+            ToastUtil.showLong(this, "Invalid email address");
+        } else if (AuthUtils.isUsernameTaken(username)) {
+            // Username is already taken
+            ToastUtil.showLong(this, "Username already taken");
+        } else if (AuthUtils.isEmailTaken(email)) {
+            // Email is already taken
+            ToastUtil.showLong(this, "Email already taken");
+        } else if (!AuthUtils.isValidUsername(username)) {
+            // Username is invalid
+            ToastUtil.showLong(this, "Invalid username");
+        } else {
+            // All fields are valid
+            return true;
+        }
 
-                } else {
-                    // Account creation failed
-                    Toast.makeText(SignUpActivity.this, "Sign Up Failed: " + task.getException().getMessage(),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-    }
-
-    private void saveUserData(String userId, String username, String email) {
-        Map<String, Object> user = new HashMap<>();
-        user.put("username", username);
-        user.put("email", email);
-
-        FirebaseFirestore.getInstance().collection("users").document(userId)
-            .set(user).addOnSuccessListener(
-                    aVoid -> Toast.makeText(SignUpActivity.this, "Sigun up success", Toast.LENGTH_SHORT).show())
-            .addOnFailureListener(
-                    e -> Toast.makeText(SignUpActivity.this, "Save data failed: " + e.getMessage(),
-                    Toast.LENGTH_SHORT).show());
+        return false;
     }
 }
