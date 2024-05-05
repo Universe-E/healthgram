8f0cabb425d16269f4769f497e61bcccb8b93a4f
Tianci <u7773219@anu.edu.au>
2024-05-06 02:05:22 +1000

    Update: trying to load friend info from database & show

--- gp/app/src/main/java/com/example/gp/home/ui/Friend/DashboardFragment.java
+++ gp/app/src/main/java/com/example/gp/home/ui/Friend/DashboardFragment.java
@@ -6,17 +6,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.gp.Adapter.NoteRecyclerViewAdapter;
 import com.example.gp.R;
+import com.example.gp.data.UserData;
 import com.example.gp.SearchActivity;
+import com.example.gp.data.Friend;
 import com.example.gp.databinding.FragmentDashboardBinding;
 import com.google.android.material.search.SearchBar;
+
+import java.util.ArrayList;
 
 /*
 * 登录成功跳转的主界面
@@ -27,9 +32,13 @@ public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
     private SearchBar searchBar;
+    private NoteRecyclerViewAdapter adapter;
+    private ArrayList<Friend> friends;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         // 加载 fragment_dashboard.xml 布局
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
@@ -41,9 +50,33 @@ public class DashboardFragment extends Fragment {
         if (searchBar != null) {
             Log.d("DashboardFragment", "成功加载 SearchBar");
             searchBar.setOnClickListener(v -> openSearchActivity());
+            Toast.makeText(getContext(), "mamba in", Toast.LENGTH_SHORT).show();
         } else {
             Log.e("DashboardFragment", "未找到 SearchBar");
+            Toast.makeText(getContext(), "mamba out", Toast.LENGTH_SHORT).show();
         }
+
+        // Initialize RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new NoteRecyclerViewAdapter(new ArrayList<>());
+        recyclerView.setAdapter(adapter);
+
+        // TODO: Set click listener to navigate to friend detail page
+        adapter.setOnItemClickListener(friend -> {
+            // open friend detail page
+        });
+
+        // TODO: get friends list from database
+        loadFriends();
+
+        // TODO: Observe friends in UserData
+//        UserData.friends().observe(getViewLifecycleOwner(), friends -> {
+//            adapter.updateFriends(friends); // Update adapter with new data
+//        });
+
+
+
 
         return view;
     }
@@ -55,5 +88,10 @@ public class DashboardFragment extends Fragment {
             searchIntent.putExtra("QUERY", initialQuery);
         }
         startActivity(searchIntent);
+    }
+
+    private void loadFriends() {
+        friends.add(new Friend("user1", "Alice", "avatar1"));
+        friends.add(new Friend("user2", "Bob", "avatar2"));
     }
 }
