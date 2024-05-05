0e7cfdfa0ce0f0f109aab16ff2996880bc13fa1e
Tianci <u7773219@anu.edu.au>
2024-05-06 02:04:45 +1000

    Initialization: add Friend.java to store friend information

---
+++ gp/app/src/main/java/com/example/gp/data/Friend.java
@@ -0,0 +1,14 @@
+package com.example.gp.data;
+
+public class Friend {
+    private String id;
+    private String nickname;
+    private String avatar;
+
+    public Friend(String id, String nickname, String avatar) {
+        this.id = id;
+        this.nickname = nickname;
+        this.avatar = avatar;
+    }
+
+}
