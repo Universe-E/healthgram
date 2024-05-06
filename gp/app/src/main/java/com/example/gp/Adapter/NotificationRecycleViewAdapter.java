7580c75dfe45a38310a31c3e4a4c47d909ea8ae7
Xingchen Zhang <43082506+Rickzhang512@users.noreply.github.com>
2024-05-06 16:36:05 +1000

    adjust on recycleview

---
+++ gp/app/src/main/java/com/example/gp/Adapter/NotificationRecycleViewAdapter.java
@@ -0,0 +1,27 @@
+package com.example.gp.Adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotificationRecycleViewAdapter  extends RecyclerView.Adapter<NoteRecyclerViewAdapter.ViewHolder> {
+
+
+
+    @NonNull
+    @Override
+    public NoteRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteRecyclerViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
