3b931513df3d11a7ce9a3bf2c9cf01eaf5275907
Han Bao <u7752342@anu.edu.au>
2024-05-07 00:39:16 +1000

    getFriendList now usable

--- gp/app/src/main/java/com/example/gp/data/Database.java
+++ gp/app/src/main/java/com/example/gp/data/Database.java
@@ -124,12 +124,13 @@ public class Database {
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             List<DocumentSnapshot> documents = task.getResult().getDocuments();
-                            List<Friend> friends = new ArrayList<>();
+                            List<Map<String, Object>> friends = new ArrayList<>();
                             for (DocumentSnapshot document : documents) {
-                                friends.add(new Friend((String) document.get("id"), (String) document.get("nickname"), (int) document.get("avatar")));
+                                // will return a list of friendMap which is a map of <String userId, Friend friend>
+                                friends.add(Map.of(document.getId(), document.toObject(Friend.class)));
                             }
                             try {
-                                MethodUtil.getMethod(object, methodName, args).invoke(object, friends);
+                                MethodUtil.invokeMethod(object, methodName, args, friends);
                             } catch (Exception e) {
                                 Log.e(TAG, "Error: " + e.getMessage());
                             }
