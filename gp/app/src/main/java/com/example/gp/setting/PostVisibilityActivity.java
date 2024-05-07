939233a51a2ea60a0d663f9e96ef072c9eab8b0b
chenyulong <u7756137@anu.edu.au>
2024-05-07 15:24:06 +1000

    call back method fixed

--- gp/app/src/main/java/com/example/gp/setting/PostVisibilityActivity.java
+++ gp/app/src/main/java/com/example/gp/setting/PostVisibilityActivity.java
@@ -2,6 +2,7 @@ package com.example.gp.setting;
 
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,17 +12,20 @@ import com.example.gp.BaseActivity;
 import com.example.gp.R;
 import com.example.gp.Items.Post;
 import com.example.gp.Utils.TimeUtil;
+import com.example.gp.Utils.ToastUtil;
 import com.example.gp.data.Database;
 import com.example.gp.databinding.ActivityPostVisibilityBinding;
 import com.example.gp.setting.Adapter.PostAdapter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class PostVisibilityActivity extends BaseActivity {
-    private static String TAG = "PVA";
+    private static String TAG = "PVA1000000";
     private com.example.gp.databinding.ActivityPostVisibilityBinding binding;
     private RecyclerView recyclerView;
     private PostAdapter mPostAdapter;
@@ -39,19 +43,32 @@ public class PostVisibilityActivity extends BaseActivity {
         recyclerView = binding.recyclerViewPosts;
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        posts = new ArrayList<>();
-        updateUI(posts);
-//        Date time = TimeUtil.getCurDate();
-//        Database.PostDB.getUserPost(time,10,this,"updateUI");
+        Date time = TimeUtil.getCurDate();
+        Database.PostDB.getUserPost(time,10,this,"updateUI");
+
+
     }
-    public void updateUI(List<Post> posts){
-        // Test Code
-        Post post_test1 = new Post("01", "02", "This is test01", "Test01", true);
-        Post post_test2 = new Post("02", "02", "This is test02", "Test02", false);
-        posts.add(post_test1);
-        posts.add(post_test2);
+
+    // Call back method
+    public void updateUI(boolean isSuccess, Object object){
+        Log.d(TAG, "updateUI");
+        if (!isSuccess) {
+            if (object == null) {
+                ToastUtil.showLong(this, "Get posts failed");
+            }
+            ToastUtil.show(this, object.toString());
+            Log.d(TAG, "success");
+        }
+
+        List<Map<String, Post>> posts = (List<Map<String, Post>>) object;
+        Log.d(TAG, posts.toString());
+        //Get posts
+        for (Map<String, Post> postMap : posts) {
+            Collection<Post> postValues = postMap.values();
+            this.posts.addAll(postValues);
+        }
         // Set post adapter
-        mPostAdapter = new PostAdapter(posts);
+        mPostAdapter = new PostAdapter(this.posts);
         recyclerView.setAdapter(mPostAdapter);
     }
 }
